ddc61f24ccc2e07b31053ec1d6766faad5a428f7
milktea <yuribling123@gmail.com>
2025-09-08 14:44:47 -0700

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
 
   return (
     <div>
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         <MessageViewer />
         <Panel />
       </div>
