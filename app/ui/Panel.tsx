5e24472705e57cc18d492bb57fb4257bd34c8dd5
milktea <yuribling123@gmail.com>
2025-09-10 20:08:29 -0700

    automation

--- app/ui/Panel.tsx
+++ app/ui/Panel.tsx
@@ -1,12 +1,24 @@
 "use client";
 import { useEffect, useState } from "react";
 import { Switch } from "@/components/ui/switch"; // shadcn switch
+import { useAutomationStore } from "@/store/AutoStore";
+import { useDeviceStore } from "@/store/DeviceStore";
+
+
 interface PanelProps {
   devices: any[]
 }
 const Panel = ({ devices }: PanelProps) => {
-  const [isEnabled, setIsEnabled] = useState(false);
-  // Find devices dynamically
+  const plug = devices.find(d => d.type === "Outlet")
+  const plug_id = plug?.deviceId
+  const enabled = useAutomationStore((s) => s.enabled);
+  const setEnabled = useAutomationStore((s) => s.setEnabled);
+
+  const device = useDeviceStore((s) => s.devices[plug_id]);
+  const updateDevice = useDeviceStore((s) => s.updateDevice);
+
+
+
 
 
   useEffect(() => {
@@ -23,14 +35,15 @@ const Panel = ({ devices }: PanelProps) => {
       }
       const payload = JSON.parse(outer.payload);
 
-      if (isEnabled && payload.event == "DoorSensor.Alert" && payload.data?.state == "closed") {
-        const plug = devices.find(d => d.type === "Outlet")
-        const plug_id = plug.deviceId
+      if (enabled && payload.event == "DoorSensor.Alert" && payload.data?.state == "closed") {
+
+        if (!plug) { return }
+
         turnOffPlug(plug_id)
       }
-      else if (isEnabled && payload.event == "DoorSensor.Alert" && payload.data?.state == "open") {
-        const plug = devices.find(d => d.type === "Outlet")
-        const plug_id = plug.deviceId
+      else if (enabled && payload.event == "DoorSensor.Alert" && payload.data?.state == "open") {
+        if (!plug) { return }
+
         turnOnPlug(plug_id)
 
       }
@@ -41,7 +54,7 @@ const Panel = ({ devices }: PanelProps) => {
     };
 
 
-  }, [isEnabled]);
+  }, [devices]);
 
 
 
@@ -50,7 +63,20 @@ const Panel = ({ devices }: PanelProps) => {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ state: "close" }),
-    });
+    })
+      .then(async (res) => {
+        const json = await res.json();
+        if (json.result.desc != "Success") {
+          throw new Error(json.result.desc);
+        }
+        updateDevice(plug_id, { state: "closed" })
+
+
+      })
+      .catch((e) => {
+
+      })
+      .finally();
   }
 
   function turnOnPlug(plugId: string) {
@@ -58,7 +84,20 @@ const Panel = ({ devices }: PanelProps) => {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ state: "open" }),
-    });
+    })
+      .then(async (res) => {
+        const json = await res.json();
+        if (json.result.desc != "Success") {
+          throw new Error(json.result.desc);
+        }
+        updateDevice(plug_id, { state: "open" })
+
+
+      })
+      .catch((e) => {
+
+      })
+      .finally();
   }
 
 
@@ -71,12 +110,12 @@ const Panel = ({ devices }: PanelProps) => {
         {/* Header */}
         <div className="flex  items-center justify-between w-full">
           <h2 className="font-semibold text-gray-900 text-base">Smart Home</h2>
-          <Switch checked={isEnabled} onCheckedChange={(value) => setIsEnabled(value)} />
+          <Switch checked={enabled} onCheckedChange={(value) => setEnabled(value)} />
         </div>
 
         {/* Status */}
         <div className=" py-2 text-sm text-gray-600">
-          {isEnabled ? "Automations are active" : "Automations are off"}
+          {enabled ? "Automations are active" : "Automations are off"}
         </div>
 
         <div className=" text-sm text-gray-600">
